47ecdccf5d12b7adac63309f48290ef6ab43fb0a
Sujal Raj <160068293+sujal862@users.noreply.github.com>
2024-12-15 22:17:11 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { AuthProvider } from './context/AuthContext'
 import axios from 'axios'
 
 // Configure axios defaults
-axios.defaults.baseURL = "https://kawach-server.onrender.com";
+axios.defaults.baseURL = "https://kawach-rnhc.onrender.com";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
